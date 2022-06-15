e81528fffa78e8c57473d690e96419fae8cb8889
Max Hegler <maxghegler@gmail.com>
2022-06-15 12:39:03 -0700

    feat(db): defined table and primary key to Pokemon model

--- app/Models/Pokemon.php
+++ app/Models/Pokemon.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Pokemon extends Model
 {
     use HasFactory;
+
+    /**
+     * The table as associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'pokemon';
+
+    /**
+     * The primary key associated with the table.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
 }
