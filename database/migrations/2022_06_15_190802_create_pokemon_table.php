4a5c83a3bee33f32317b859419238d929186d600
Max Hegler <maxghegler@gmail.com>
2022-06-15 12:26:17 -0700

    feat(db): added columns to migration script for table "pokemon"

--- database/migrations/2022_06_15_190802_create_pokemon_table.php
+++ database/migrations/2022_06_15_190802_create_pokemon_table.php
@@ -15,7 +15,18 @@ class CreatePokemonTable extends Migration
     {
         Schema::create('pokemon', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('name');
+            $table->string('type_1');
+            $table->string('type_2');
+            $table->integer('total');
+            $table->integer('hp');
+            $table->integer('attack');
+            $table->integer('defense');
+            $table->integer('sp_atk');
+            $table->integer('sp_def');
+            $table->integer('speed');
+            $table->integer('generation');
+            $table->boolean('legendary');
         });
     }
 
