1d5796941af050980366861c4febbf54205916a5
Max Hegler <maxghegler@gmail.com>
2022-06-15 17:45:24 -0700

    feat(api): added low-level functions for sending JSON responses

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,34 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    /**
+     * Issue a success JSON response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendSuccess($result, $message) {
+        $response = [
+            'success' => true,
+            'data' => $result,
+            'message' => $message,
+        ];
+
+        return response()->json($response, 200);
+    }
+
+    /**
+     * Issue an error JSON response.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function sendError($message, $status = 401) {
+        $response = [
+            'success' => false,
+            'message' => $message,
+        ];
+
+        return response()->json($response, $status);
+    }
+
 }
